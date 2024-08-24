97606503dbe487ebe6b592332eb7bc1916cac59d
raj patel <rajpatel8726@gmail.com>
2024-08-24 17:18:00 +0530

    add pages

--- scriptnavbar.js
+++ scriptnavbar.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
       headerContainer.innerHTML = `
         <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-            <a href="" class="navbar-brand p-0">
+            <a href="index.html" class="navbar-brand p-0">
                 <h4 class="text-primary m-0"><i class="fa fa-map-marker-alt me-3"></i>SwariTravels</h4>
                 <!-- <img src="img/logo.png" alt="Logo"> -->
             </a>
