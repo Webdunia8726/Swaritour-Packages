31ab163e33a0c08e760402752366cb097ba49d76
raj patel <rajpatel8726@gmail.com>
2024-09-16 12:46:25 +0530

    sari

--- scriptnavbar.js
+++ scriptnavbar.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
             <a href="index.html" class="navbar-brand p-0">
-                <h4 class="text-primary m-0"><i class="fa fa-map-marker-alt me-3"></i>SwariTravels</h4>
+                <h4 class="text-primary m-0"><img src="img/SWARI-LOGO (1).png" alt=""></h4>
                 <!-- <img src="img/logo.png" alt="Logo"> -->
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
